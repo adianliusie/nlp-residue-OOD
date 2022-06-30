4f338bcc3cbce8906ad5c5910fbd8823356a2559
Vyas Raina <sayvaniar@gmail.com>
2022-06-30 23:50:25 +0100

    kmeans selector main alg in place

--- kmeans_selector.py
+++ kmeans_selector.py
@@ -1,6 +1,9 @@
 import torch 
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import numpy as np
 
 class KMeansSelector():
     '''
@@ -9,10 +12,35 @@ class KMeansSelector():
         K-Means clustering
         Select K samples (closest to each cluster mean)
     '''
-    @staticmethod
-    def select(model, ):
-        pass
+    @classmethod
+    def select(cls, input_ids, mask, model, device, bs=8, ncomps=10, frac_samples=0.1):
 
+        # Encoder embedding space
+        embeddings = cls._get_encoder_embeddings(input_ids, mask, model, device, bs).detach().cpu().numpy()
+
+        # PCA compression
+        pca = PCA(n_components=ncomps)
+        compressed = pca.fit_transform(embeddings)
+
+        # K means clustering
+        num_samples = int(frac_samples*embeddings.size(0))
+        kmeans = KMeans(n_clusters=num_samples, random_state=0).fit(compressed)
+        
+        # Select closest to mean per cluster
+        cluster_means = kmeans.cluster_centers_
+        cluster_labels = kmeans.cluster_labels_
+        selected_samples = []
+        for k in num_samples:
+            valid_samples = compressed[cluster_labels==k]
+            valid_mean = cluster_means[k]
+            diff_l2 = np.linalg.norm(valid_samples - valid_mean, axis=1)
+            ind = np.argmin(diff_l2)
+            selected_samples.append(valid_samples[ind])
+        return np.stack(selected_samples)
+
+
+
+    @staticmethod
     def _get_encoder_embeddings(input_ids, mask, model, device, bs=8):
         '''
         Input is a tensor of input ids and mask
