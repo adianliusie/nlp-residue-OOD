d52fb2e56c6bfe549a0ccdd87d0a02174962ed10
Adian Liusie <adianliusie98@gmail.com>
2022-08-09 06:06:17 +0100

    working OOD model detection frameowrk (residue + max prob)

--- src/analysis/model_analyser.py
+++ src/analysis/model_analyser.py
@@ -1,6 +1,9 @@
+import torch
+
 from transformers import BertModel
 from tqdm import tqdm
 from sklearn.decomposition import PCA
+from functools import lru_cache
 
 from ..system_loader import SystemLoader
 from ..utils.torch_utils import no_grad
@@ -14,6 +17,7 @@ class ModelAnalyser(SystemLoader):
     def reset_model(self):
         self.model  = BertModel.from_pretrained("bert-base-uncased")
     
+    @lru_cache(maxsize = 5)
     @no_grad
     def get_cls_vectors(self, data_name:str, mode:str, lim:int=None, quiet=False)->dict:
         """get model predictions for given data"""
@@ -30,6 +34,24 @@ class ModelAnalyser(SystemLoader):
             h = output.h.squeeze(0)
             CLS_vectors[sample_id] = h.cpu().numpy()
         return CLS_vectors
+    
+    @no_grad
+    def get_probs(self, data_name:str, mode:str, lim:int=None, quiet=False)->dict:
+        self.model.eval()
+        self.to(self.device)
+        eval_data = self.data_loader.get_data_split(data_name, mode, lim)
+        eval_batches = self.batcher(data=eval_data, bsz=1, shuffle=False)
+        
+        output_probs = {}
+        for batch in tqdm(eval_batches, disable=quiet):
+            sample_id = batch.sample_id[0]
+            output = self.model_output(batch)
+
+            y = output.y
+            prob = torch.softmax(y, dim=-1).squeeze(0)
+            output_probs[sample_id] = prob.cpu().numpy()
+            
+        return output_probs        
     
     def get_hits(self, data_name:str, mode:str)->dict:
         preds = self.load_preds(data_name, mode='test')
@@ -49,5 +71,5 @@ class PcaAnalyser(ModelAnalyser):
         matrix = [i for i in cls_vectors.values()]
         output = self.pca.transform(matrix)
         return output
-            
+        
         
